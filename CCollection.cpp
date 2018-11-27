01115bf9fadd2f848f78bffab2dfa5f6c5c0f8e9
Jeroen Geel <jeroen.geel@hotmail.com>
2018-11-27 19:41:02 +0100

    Added files for a general collection.

---
+++ CCollection.cpp
@@ -0,0 +1,6 @@
+#include "CCollection.h"
+
+CCollection::CCollection()
+{
+
+}
---
+++ CCollection.h
@@ -0,0 +1,11 @@
+#ifndef CCOLLECTION_H
+#define CCOLLECTION_H
+
+
+class CCollection
+{
+public:
+    CCollection();
+};
+
+#endif // CCOLLECTION_H
